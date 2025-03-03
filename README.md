6637fdb1c2b11c03b8802de84a40473247d53375
MacDaddy1660B <gordon.andrew.macdonald@gmail.com>
2025-03-03 09:59:37 -0700

    Updating docstrings and readme.

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # pyPixel16
 
+Author: Gordon A. MacDonald, PhD, Apache Point Observatory, NM
+
 This is a collection of functions that perform bit- and bitewise operations of byte strings of unsigned shorts (16-bits on most systems). It performs the operations using Numpy. This package will replace the [pixel16](https://github.com/ApachePointObservatory/pixel16.git) wrapper (author unknown).
 
-1. ```uflip```: flips the sign bits in a string of unsigned short ints.
+1. ```uflip```: flips the most significant bits in a string of unsigned short ints.
 2. ```byteswap```: swaps bytes in a string of unsigned short ints.
+
+Beware that any byte string will always be interpreted as a string of 16-bit unsigned ints (unsigned short). If the original bytes were derived from char, int32, or anything else, these will be read-in and processed as if they were unsigned shorts.
+
+## Installation
+
+```
+python3 -m build
+pip3 install build/pyPixel16-0.0.1.whl
+```
+
+## How to use pyPixel16
+
+Below is a code snippet demonstrating the use of pyPixel16 in the interpreter.
+
+```
+>>> import pyPixel16
+>>> pyPixel16.byteswap(b"abcd")
+b'badc'
+>>> pyPixel16.uflip(b"\x00\xff\x07\x80")
+b'\x00\x7f\x07\x00'
+>>> pyPixel16.uflip(b"\x00\xff\x07\x80\x01") ## Input must have an even-number of bytes.
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/gmac/Documents/dev/pyPixel16/pyPixel16/pixel16.py", line 48, in uflip
+    a = np.frombuffer(b, dtype=np.ushort)
+ValueError: buffer size must be a multiple of element size
+>>> pyPixel16.uflip("abcd")	## Input must be bytes-like
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/gmac/Documents/dev/pyPixel16/pyPixel16/pixel16.py", line 48, in uflip
+    a = np.frombuffer(b, dtype=np.ushort)
+TypeError: a bytes-like object is required, not 'str'
+```
--- pyPixel16/pixel16.py
+++ pyPixel16/pixel16.py
@@ -7,14 +7,85 @@ __all__ = [
         "uflip",
         ]
 
-def uflip(image):
-    a = np.frombuffer(image, dtype=np.ushort)
+def uflip(b):
+    """
+    Flips the most significant bit of an unsigned short, b
+    by performing the bitwise XOR b ^ 0x8000.
+
+    Parameters
+    ----------
+        bytes-like 
+            A string of an even number of bytes.
+            This string will be interpreted
+            as a string of 16-bit unsigned int
+            (i.e. unisgned short).
+
+    Returns
+    -------
+        bytes
+            A string of an even number of bytes
+            with the most significant bit of each
+            16-bit byte pair flipped.
+
+    Raises
+    ------
+        TypeError
+            b is not bytes-like.
+
+        ValueError
+            b holds an odd number of bytes.
+
+    Examples
+    --------
+        >>> pyPixel16.uflip(b"\\x00\\x8f\\xff\\x70")
+        b'\\x00\\x0f\\xff\\xf0'
+
+        First byte unchanged
+        Second byte flipped from 0x80 to 0x00
+        Third byte unchanged
+        Fourth byte flipped 0x70 to 0xf0
+    """
+    a = np.frombuffer(b, dtype=np.ushort)
     a = __do_uflip(a)
     return a.tobytes()
 
 
-def byteswap(image):
-    a = np.frombuffer(image, dtype=np.ushort)
+def byteswap(b):
+    """
+    Swaps bytes of an unsigned short int.
+
+    Parameters
+    ----------
+        b : bytes-like:
+            A string of an even number of bytes.
+            This string will be interpreted
+            as a string of 16-bit unsigned int
+            (i.e. unsigned short).
+
+    Returns
+    -------
+        bytes
+            A string of an even number of bytes
+            with byte order swapped.
+
+    Raises
+    ------
+        TypeError
+            b is not bytes-like.
+
+        ValueError
+            b holds an odd number of bytes.
+
+    Examples
+    --------
+        >>> pyPixel16.byteswap(b"\\x00\\x8f\\xff\\x70")
+        b'\\x8f\\x00p\\xff'
+
+        The byte order of the first + second bytes
+        and the third + fourth bytes are swapped. Note
+        \\x70 is encoded as char 'p'.
+    """
+    a = np.frombuffer(b, dtype=np.ushort)
     a = __do_byteswap(a)
     return a.tobytes()
 
